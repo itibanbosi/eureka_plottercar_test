af197911cf643205a58a804659bb2c44bd0a70f7
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2021-03-22 23:41:12 +0900

    更新 plottercar_test.ts

--- plottercar_test.ts
+++ plottercar_test.ts
@@ -117,7 +117,7 @@ function  moter()
   export function microbit_version_info(Version_info : microbit_version) {
     switch(Version_info){
         case microbit_version.Version1:
-        microbit_wait=700;
+        microbit_wait=800;
         break;
         case microbit_version.Version2:
         microbit_wait=7000;
