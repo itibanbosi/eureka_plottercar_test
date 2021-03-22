eb121a6d05a51d66087b0e1e55a90701e2a9c1a9
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2021-03-22 23:43:01 +0900

    更新 plottercar_test.ts

--- plottercar_test.ts
+++ plottercar_test.ts
@@ -75,7 +75,7 @@ let Stepping_L = [
 
 let moter_number=0;
 
-//% color="#3943c6" block="ﾌﾟﾛｯﾀｰ・ｶｰVer1.2" icon="\uf1b9"
+//% color="#3943c6" block="ﾌﾟﾛｯﾀｰ・ｶｰVer1.4" icon="\uf1b9"
 
 namespace eureka_plotter_car {
 
